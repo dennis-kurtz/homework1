5e3e8545ce32834297327b9d39454b2cd548562a
Hans Johnson <hans.j.johnson@gmail.com>
2020-09-08 12:12:09 -0500

    Initialize provide-clean-code-starting-point

---
+++ lib/ruby_intro.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+# When done, submit this entire file to the autograder.
+
+# Part 1
+
+def sum(arr)
+  # TODO: YOUR CODE HERE
+end
+
+def max_2_sum(arr)
+  # TODO: YOUR CODE HERE
+end
+
+def sum_to_n?(arr, n)
+  # TODO: YOUR CODE HERE
+end
+
+# Part 2
+
+def hello(name)
+  # TODO: YOUR CODE HERE
+end
+
+def starts_with_consonant?(s)
+  # TODO: YOUR CODE HERE
+end
+
+def binary_multiple_of_4?(s)
+  # TODO: YOUR CODE HERE
+end
+
+# Part 3
+
+class BookInStock
+# TODO: YOUR CODE HERE
+end
